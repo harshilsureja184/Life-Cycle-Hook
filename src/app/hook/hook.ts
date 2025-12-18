798be27bf3e78b36cb39e589b41dd52df94f675c
Harshil Sureja <harshilsureja184@gmail.com>
2025-12-18 16:08:11 +0530

    update

--- src/app/hook/hook.ts
+++ src/app/hook/hook.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-hook',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './hook.html',
   styleUrl: './hook.css',
 })
-export class Hook {
+export class Hook implements OnInit {
 
   labelName = 'Life Cycle Hook';
 
